ba977be713f6167cd92a4e56e42a082e02b7c099
Rob Bruinsma <gusbruiro@student.gu.se>
2012-03-30 10:35:02 +0200

    Updated code, not finished yet

--- pressure.c
+++ pressure.c
@@ -7,8 +7,13 @@
 
 float getPressure() {
 
-  float pr_kPa; // Pressure in kPa
-  pr_kPa = ((float) analogRead(A0)/1023+0.04)/0.004; // Takes analog output and converts it into kPa
-  return pr_kPa;
+  float pr_inside; // Pressure in kPa inside of the skirt
+  pr_inside = ((float) analogRead(A0)/1023+0.04)/0.004; // Takes analog output and converts it into kPa
+
+  float pr_outside; // Pressure in kPa on the outside of the hovercraft
+  pr_outside = ((float) analogRead(A0)/1023+0.04)/0.004;
+
+  float pr_dif = pr_inside - pr_outside;
+  return pr_dif;
 
 }
