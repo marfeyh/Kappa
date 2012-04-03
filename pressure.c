a547000432e72734e8251f1a0d1994c2ef4241d2
Rob Bruinsma <gusbruiro@student.gu.se>
2012-04-03 14:17:40 +0200

    Changed back to returning the difference in pressure

--- pressure.c
+++ pressure.c
@@ -5,18 +5,13 @@
   Gets pressure info from pressure sensor, convert it to kPa.
  */
 
-float getPressureIn() { // For the sensor in the skirt
+float getPressure() { // For the sensor in the skirt
 
-  float pr_kPa; // Pressure in kPa inside of the skirt
-  pr_kPa = ((float) analogRead(A0)/1023+0.04)/0.004; // Takes output of analog port 0 and converts it into kPa
+  float pr_inside; // Pressure in kPa inside of the skirt
+  pr_inside = ((float) analogRead(A0)/1023+0.04)/0.004; // Takes output of analog port 0 and converts it into kPa
 
-  return pr_kPa;
-}
+  float pr_outside; // Pressure in kPa on the outside of the hovercraft
+  pr_outside = ((float) analogRead(A1)/1023+0.04)/0.004; // Takes output of analog port 1 and converts it into kPa
 
-float getPressureOut() { // For the sensor on the outside
-
-float pr_kPa2; // Pressure in kPa on the outside of the hovercraft
-  pr_kPa2 = ((float) analogRead(A1)/1023+0.04)/0.004; // Takes output of analog port 1 and converts it into kPa
-
-  return pr_kPa2;
+  return pr_inside - pr_outside;
 }
